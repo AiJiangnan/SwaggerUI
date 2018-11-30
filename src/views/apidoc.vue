58d4bca1f647fbc47578472d48b121fb69db3cf9
AiJiangnan <904629998@qq.com>
2018-11-30 14:59:41 +0800

    [add] 添加返回值

--- src/views/apidoc.vue
+++ src/views/apidoc.vue
@@ -2,36 +2,91 @@
   <div id="data">
     <table cellspacing="0" cellpadding="0" border="0">
       <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
+        <td class="data-label">接口分类：</td>
+        <td colspan="4">{{api.tags.join()}}</td>
       </tr>
       <tr>
-        <td>5</td>
-        <td>6</td>
-        <td>7</td>
-        <td>8</td>
+        <td class="data-label">接口名称：</td>
+        <td colspan="4">{{api.summary}}</td>
       </tr>
       <tr>
-        <td>9</td>
-        <td>0</td>
-        <td>1</td>
-        <td>2</td>
+        <td class="data-label">请求地址：</td>
+        <td colspan="4"><code>{{url}}</code></td>
       </tr>
       <tr>
-        <td>3</td>
-        <td>4</td>
-        <td>2</td>
-        <td>1</td>
+        <td class="data-label">请求方式：</td>
+        <td colspan="4"><code>{{method}}</code></td>
+      </tr>
+      <tr>
+        <td colspan="5"></td>
+      </tr>
+      <tr>
+        <td colspan="5" class="data-label">入口参数说明：<code>Content-Type: {{api.consumes.join(';')}}</code></td>
+      </tr>
+      <tr>
+        <td class="data-head">参数名称</td>
+        <td class="data-head">数据类型</td>
+        <td class="data-head">参数描述</td>
+        <td class="data-head">是否必须</td>
+        <td class="data-head">参数类型</td>
+      </tr>
+      <tr v-for="param in api.parameters">
+        <td class="data-info">{{param.name}}</td>
+        <td class="data-info">{{getDefinitionType(param)}}</td>
+        <td class="data-info">{{param.description}}</td>
+        <td class="data-info">{{param.required?'是':'否'}}</td>
+        <td class="data-info">{{param.in}}</td>
+      </tr>
+      <tr>
+        <td colspan="5"></td>
+      </tr>
+      <tr>
+        <td colspan="5" class="data-label">返回结果说明：<code>Content-Type: {{api.produces.join(';')}}</code></td>
+      </tr>
+      <tr>
+        <td class="data-head">参数名称</td>
+        <td class="data-head">数据类型</td>
+        <td colspan="3" class="data-head">参数描述</td>
+      </tr>
+      <tr v-for="(val,key) in getDefinition(api.responses['200'])" v-if="val.type && val.type!=='array'">
+        <td class="data-info">{{key}}</td>
+        <td class="data-info">{{val.type}}</td>
+        <td colspan="3" class="data-info">{{val.description}}</td>
+      </tr>
+      <tbody v-else>
+      <tr>
+        <td class="data-info">{{key}}</td>
+        <td class="data-info">{{getSchemaType(val)}}</td>
+        <td colspan="3" class="data-info">{{val.description}}</td>
+      </tr>
+      <tr v-for="(sVal,sKey) in getDefinitionResp(val)">
+        <td class="data-info" style="padding-left:30px;">{{sKey}}</td>
+        <td class="data-info">{{sVal.type}}</td>
+        <td colspan="3" class="data-info">{{sVal.description}}</td>
+      </tr>
+      </tbody>
+      <tr>
+        <td colspan="5"></td>
+      </tr>
+      <tr>
+        <td colspan="5" class="data-label">响应状态码：</td>
+      </tr>
+      <tr>
+        <td class="data-head">状态码</td>
+        <td class="data-head">描述</td>
+        <td class="data-head">响应类型</td>
+        <td class="data-head">响应内容</td>
+        <td class="data-head">响应头</td>
+      </tr>
+      <tr v-for="(val,key) in api.responses">
+        <td class="data-info">{{key}}</td>
+        <td class="data-info">{{val.description}}</td>
+        <td class="data-info">{{getDefinitionType(val)}}</td>
+        <td class="data-info"></td>
+        <td class="data-info"></td>
       </tr>
     </table>
   </div>
-  <!--<div>
-    <h2>{{url}}</h2>
-    <p>{{method}}</p>
-    <pre>{{api}}</pre>
-  </div>-->
 </template>
 
 <script>
@@ -47,6 +102,45 @@
       return {
         definitions: JSON.parse(sessionStorage.definitions)
       }
+    },
+    methods: {
+      getRefName: function (ref) {
+        return ref.replace('#/definitions/', '');
+      },
+      getSchemaType: function (schema) {
+        if (!schema) return;
+        if (schema.type === 'array') {
+          let items = schema.items;
+          return 'List<' + (items.type ? items.type : this.getRefName(items.$ref)) + '>';
+        } else {
+          return this.getRefName(schema.$ref);
+        }
+      },
+      getDefinitionType: function (param) {
+        if (param.type) {
+          return param.type;
+        }
+        return this.getSchemaType(param.schema);
+      },
+      getDefinition: function (param) {
+        const ref = this.getRefName(param.schema.$ref);
+        const definition = this.definitions[ref];
+        return definition.properties;
+      },
+      getDefinitionResp: function (val) {
+        if (val.type === 'array') {
+          let items = val.items;
+          if (items.type) {
+            return;
+          }
+          return this.definitions[this.getRefName(items.$ref)].properties;
+        } else {
+          return this.definitions[this.getRefName(val.$ref)].properties;
+        }
+      }
+    },
+    mounted() {
+      console.log(this.api);
     }
   }
 </script>
@@ -65,6 +159,20 @@
   #data td {
     padding: 8px;
     border: 1px solid;
+  }
+
+  .data-label {
+    width: 3cm;
+    background: #eee;
+    font-weight: bold;
+  }
+
+  .data-head {
+    font-weight: bold;
+  }
+
+  .data-head {
+    text-align: center;
   }
 
 </style>
